4b20633a5947495dbd829f16a84d856166fe5df0
Bogapro67 <bernalbogarin@gmail.com>
2026-05-21 17:26:37 -0700

    agregue cosas nuevas al admin .py

--- catalogo/admin.py
+++ catalogo/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Marca, Categoria, Vendedor, Tenis, Cliente, Venta
+from .models import Marca, Categoria, Vendedor, Tenis, Cliente, Venta, Material
 
 admin.site.register(Marca)
 admin.site.register(Categoria)
@@ -7,3 +7,4 @@ admin.site.register(Vendedor)
 admin.site.register(Tenis)
 admin.site.register(Cliente)
 admin.site.register(Venta)
+admin.site.register(Material) # <-- Clave para agregar materiales desde el panel admin
