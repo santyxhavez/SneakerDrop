db3e0791392d0be86733c87552c28a84a1d9a59b
megaalex507-cloud <megaalex507@gmail.com>
2026-05-25 16:01:32 -0700

    Añadi a catalogo urls y catalogo views

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Tenis
 
-# Create your views here.
+def home(request):
+    busqueda = request.GET.get('buscar')
+    if busqueda:
+        lista_tenis = Tenis.objects.filter(modelo__icontains=busqueda)
+    else:
+        lista_tenis = Tenis.objects.all()
+    return render(request, 'catalogo/index.html', {'lista_tenis': lista_tenis, 'busqueda': busqueda})
+
+def detalle(request, tenis_id):
+    # Traemos el par de tenis y Django automáticamente incluirá sus materiales mapeados
+    tenis = get_object_or_404(Tenis, pk=tenis_id)
+    return render(request, 'catalogo/detalle_tenis.html', {'tenis': tenis})
